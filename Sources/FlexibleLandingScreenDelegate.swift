f6a885b40e5b60edcccc87586bb2489938b38585
Den Andreychuk <denis.andrei4uk@gmail.com>
2023-01-11 16:44:26 +0200

    Make `FlexibleLandingScreenDelegate` public

--- Sources/FlexibleLandingScreenDelegate.swift
+++ Sources/FlexibleLandingScreenDelegate.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol FlexibleLandingScreenDelegate: AnyObject {
+public protocol FlexibleLandingScreenDelegate: AnyObject {
     func finishScreenFlow(for screen: FlexibleLandingScreen, animated: Bool)
     func finishLandingFlow(animated: Bool)
     func didCommitPurchase(of product: String, result: FlexibleLandingCommitPurchaseResult, screen: FlexibleLandingScreen)
