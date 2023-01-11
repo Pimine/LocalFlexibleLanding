7e4875897e9d7fc341551cd726aa99657fed28de
Den Andreychuk <denis.andrei4uk@gmail.com>
2023-01-11 15:28:18 +0200

    Initial Commit

---
+++ LocalFlexibleLanding/LocalFlexibleLanding.h
@@ -0,0 +1,18 @@
+//
+//  LocalFlexibleLanding.h
+//  LocalFlexibleLanding
+//
+//  Created by Den Andreychuk on 11.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalFlexibleLanding.
+FOUNDATION_EXPORT double LocalFlexibleLandingVersionNumber;
+
+//! Project version string for LocalFlexibleLanding.
+FOUNDATION_EXPORT const unsigned char LocalFlexibleLandingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFlexibleLanding/PublicHeader.h>
+
+
